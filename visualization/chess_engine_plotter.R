b712283d402c29300489a028dba2f0239b80832c
Sculps <nickeagles77@gmail.com>
2020-05-13 20:49:54 -0400

    Use most recent loss plotting settings

--- visualization/chess_engine_plotter.R
+++ visualization/chess_engine_plotter.R
@@ -1,20 +1,21 @@
 library(ggplot2)
 
-#setwd("C:/Users/Nick/Appdata/Local/Programs/Python/Python37/Scripts/Chess_Engine/visualization")
-setwd("C:/Users/Nick/Appdata/Local/Programs/Python/Python37/Scripts/MNIST_chess/visualization")
+setwd("C:/Users/Nick/Appdata/Local/Programs/Python/Python37/Scripts/Chess_Engine/visualization")
+#setwd("C:/Users/Nick/Appdata/Local/Programs/Python/Python37/Scripts/MNIST_chess/visualization")
 costData = read.csv(file="costs.csv")
 costData$isStart = as.factor(costData$isStart)
 
-#costData = tail(costData, 80)
-costData = costData[3:nrow(costData),]
+costData = tail(costData, 400)
+#costData = costData[costData$costType == 'v_cost',]
+#costData = costData[5:nrow(costData),]
 ggplot(costData) +
   geom_point(mapping = aes(x=epochNum, y=cost, color=costType, shape=isStart)) +
   labs(title="Cost on data vs. epoch", color="loss type", shape="starting loss?", x="epoch number") +
   scale_color_manual(labels=c("training", "validation"), values=c("#EE1111","#11CCEE")) +
-  scale_shape_manual(labels=c("False", "True"), values=c(16, 2)) #+
-  #geom_smooth(data = costData[costData$costType == 'v_cost' & costData$isStart == 0,],
-              #mapping = aes(x=epochNum, y=cost),
-              #method='lm')
+  scale_shape_manual(labels=c("False", "True"), values=c(16, 2)) +
+  geom_smooth(data = costData[costData$costType == 'v_cost' & costData$isStart == 0,],
+              mapping = aes(x=epochNum, y=cost),
+              method='lm')
 
 #############################################################
 #   Some stats you can perform, optionally, on the data
