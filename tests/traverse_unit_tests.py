e08bbff2ffb614bba9d4ed4b07ebfd11367265e1
Sculps <nickeagles77@gmail.com>
2020-12-13 22:06:06 -0500

    Add another case/ unit test for Traversals

--- tests/traverse_unit_tests.py
+++ tests/traverse_unit_tests.py
@@ -57,7 +57,8 @@ session.Load('nets/' + net_name, lazy=True)
 print("Testing 'baseR' attribute of a traversal object (1)...")
 expected_r1 = 0
 g = game.copy()
-file_IO.toFEN(g.toNN_vecs(every=False)[0], 'visualization/misc_tests/traverse_unit_tests.fen')
+file_IO.toFEN(g.toNN_vecs(every=False)[0],
+              'visualization/misc_tests/traverse_unit_tests1.fen')
 
 m = Move.Move((2, 7), (7, 7), 4) # Rh8+
 expected_r1 += g.getReward(m, p['mateReward'], simple=True)[0]
@@ -149,6 +150,74 @@ assert best_eval != 0, best_eval
 #   Actually perform the traversal
 trav = Traversal.Traversal(game, session.net, p)
 trav.traverse()
+session.net.certainty = temp
 
 print("Testing 'baseR' attribute of a traversal object (3)...")
 misc.expect_equal(best_eval, trav.baseR, is_float=True)
+
+################################################################################
+#   Test trav.baseR: position 4 (finding the optimal sequence; inclusion of
+#   NN eval)
+################################################################################
+
+#   Manually produce a particular position
+game = Game.Game()
+game.board = [[0] * 8,
+              [0] * 8,
+              [0] * 6 + [3, 0],
+              [0] * 8,
+              [0] * 8,
+              [4] + [0] * 7,
+              [0, 1, -1, -1, -4, 0, 0, 0],
+              [6, -1, 0, -6] + [0] * 4]
+game.invBoard = board_helper.invert(game.board)
+game.wPieces = [2, 0, 0, 1, 1, 0]
+game.bPieces = [3, 0, 0, 0, 0, 1]
+game.canW_K_Castle = False
+game.canW_Q_Castle = False
+game.canB_K_Castle = False
+game.canB_Q_Castle = False
+game.updateValues()
+
+temp = session.net.certainty
+session.net.certainty = 0.14
+
+p['depth'] = 3
+p['breadth'] = 21
+p['gamma_exec'] = 0.9
+p['mateReward'] = 3
+p['minCertainty'] = 0.0
+
+#   Visually show that it is as expected
+file_IO.toFEN(game.toNN_vecs(every=False)[0],
+              'visualization/misc_tests/traverse_unit_tests4.fen')
+
+#   Perform the optimal sequence manually, then measure reward (including NN
+#   eval)
+g = game.copy()
+
+num_moves = len(board_helper.getLegalMoves(g))
+assert num_moves == 21, num_moves
+
+m = Move.Move((2, 6), (3, 7), 3)
+assert m.getMoveName(g.board) == 'Bd8'
+g.doMove(m)
+
+m = Move.Move((7, 3), (7, 4), -6)
+assert m.getMoveName(g.board) == 'Kh5'
+g.doMove(m)
+
+m = Move.Move((3, 7), (6, 4), 3)
+assert m.getMoveName(g.board) == 'Bxg5'
+r, vec = g.getReward(m, p['mateReward'])
+
+expected_r = p['gamma_exec']**2 * r + \
+             p['gamma_exec']**3 * session.net.certainty * float(logit(session.net(vec, training=False)))
+
+#   Confirm the traversal yields the same baseR
+trav = Traversal.Traversal(game, session.net, p)
+trav.traverse()
+session.net.certainty = temp
+
+print("Testing 'baseR' attribute of a traversal object (4)...")
+misc.expect_equal(expected_r, trav.baseR, is_float=True)
