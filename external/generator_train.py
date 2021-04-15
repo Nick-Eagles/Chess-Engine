a9784e0659846173cf8abc8541c83ad3b8289fcd
Sculps <nickeagles77@gmail.com>
2021-04-15 19:57:32 -0400

    Save the model along with a valid certainty metric; record latest accuracy achieved along with the hyperparams used

--- external/generator_train.py
+++ external/generator_train.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 import random
 import _pickle as pickle
 import gzip
@@ -12,17 +13,22 @@ sys.path.append('./experimental/')
 import input_handling
 import policy_net
 import read_pgn
+import Session
+import q_learn
+import buffer as Buffer
 
 t_dir = 'external/2019_games_t'
 v_x_path = 'external/2019_games_tensor_v_x.pkl.gz'
 v_y_path = 'external/2019_games_tensor_v_y.pkl.gz'
 loss_path = 'visualization/external_generator_costs.csv'
+hyper_path = 'external/gen_hyperparams.csv'
+net_dir = 'nets/tf_ex_gen_deep'
 num_groups = 2
-blocks_per_group = 1
+blocks_per_group = 2
 block_width = 2
-n = 308400
+n = 776000
 
-def load_v_data(path):
+def load_data(path):
     with gzip.open(path, 'rb') as f:
         data = pickle.load(f)
 
@@ -36,7 +42,7 @@ net = policy_net.InitializeNet(num_groups,
                                'policy_value')
 
 print('Loading validation data...')
-v_data = (load_v_data(v_x_path), load_v_data(v_y_path))
+v_data = (load_data(v_x_path), load_data(v_y_path))
 
 print('Compiling model...')
 optim = tf.keras.optimizers.Adam(learning_rate=p['nu'])
@@ -53,3 +59,56 @@ new_history = net.fit(
     validation_data = v_data,
     callbacks=[csv_logger]
 )
+
+print('Reshaping data for computing certainty...')
+
+#   Format external data as a valid buffer
+buffer = [[]]
+for i in range(v_data[0].shape[0]):
+    #   Get this particular example as a tuple (input, label)
+    x = tf.reshape(v_data[0][i, :], [1, 839])
+    y = [tf.reshape(v_data[1][0][i, :], [1, 64]),
+         tf.reshape(v_data[1][1][i, :], [1, 64]),
+         tf.reshape(v_data[1][2][i, :], [1, 6]),
+         tf.reshape(v_data[1][3][i, :], [1, 1])]
+    
+    buffer[0].append((x, y))
+
+Buffer.verify(buffer, p, numBuffs=1)
+
+#   Compute certainty and update model attributes
+print('Computing certainty...')
+p['persist'] = 0
+q_learn.getCertainty(net, buffer, p, greedy=False)
+net.certaintyRate = 0
+
+#   Save network without any associated data
+print('Saving model...')
+session = Session.Session([[]], [[]], net)
+session.Save(net_dir)
+
+#   Record hyperparams and latest accuracy achieved
+print('Recording results...')
+lay_indices = [1 + (2 + 2 * blocks_per_group * block_width) * i
+               for i in range(num_groups)]
+lay_lens = [net.layers[i].output_shape[-1] for i in lay_indices]
+lay_lens_str = ','.join([str(x) for x in lay_lens])
+
+last_acc = round(new_history.history['val_policy_end_square_categorical_accuracy'][-1],
+                 4)
+
+hyperparams = [[num_groups,
+               blocks_per_group,
+               block_width,
+               lay_lens_str,
+               p['nu'],
+               p['batchSize'],
+               p['weightDec'],
+               p['epochs'],
+               last_acc]]
+
+with open(hyper_path, 'a') as f:
+    writer = csv.writer(f)
+    writer.writerows(hyperparams)
+
+print('Done.')
