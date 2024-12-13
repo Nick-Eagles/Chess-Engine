2401d2eff60f9b1e3095bdeb656d878b37ede8a2
Nick-Eagles <nickeagles77@gmail.com>
2024-12-12 19:46:13 -0500

    Add code to plot training + test metrics on the semi-synthetic dataset

--- external/fit_test_data.py
+++ external/fit_test_data.py
@@ -13,10 +13,12 @@ import pickle
 import gzip
 import pandas as pd
 import re
+from plotnine import ggplot, aes, geom_line, coord_cartesian, theme_bw, ggsave, facet_wrap, labs
 
 sys.path.append(str(here()))
 
 data_path = here('external', 'tensor_list.pkl.gz')
+metrics_plot_path = here('visualization', 'test_metrics.pdf')
 hidden_layer_lens = [200, 100, 50]
 policy_weight = 0.5
 optimizer = 'adam'
@@ -112,24 +114,49 @@ history_df.index.name = 'epoch'
 history_df.reset_index(inplace = True)
 
 #   Reformat longer, such that as columns we have 'epoch', 'data_group' (train
-#   or val), and then each tracked metric
+#   or val), metric name, and value for that metric
 history_df = pd.melt(
     history_df,
     id_vars = 'epoch',
-    value_vars = history_df.drop('epoch', axis = 1).columns
+    value_vars = history_df.drop('epoch', axis = 1).columns,
+    value_name = 'categorical_accuracy'
 )
 history_df['data_group'] = history_df['variable'].str.extract('^(train|val)')
 history_df['metric'] = history_df['variable'].apply(
     lambda x: re.sub('^(train|val)_', '', x)
 )
-history_df = (
-    history_df
-        .drop('variable', axis = 1)
-        .pivot(
-            index = ['epoch', 'data_group'],
-            columns = 'metric',
-            values = 'value'
-        )
-        .reset_index()
+history_df.drop('variable', axis = 1, inplace = True)
+
+#   Now only take the categorical accuracy metrics and construct a column
+#   replacing 'metric' that just has the output-layer name (e.g. 
+#   'policy_end_piece')
+history_df = history_df[
+    history_df['metric'].str.match(r'.*_categorical_accuracy$')
+]
+history_df['output_type'] = history_df['metric'].apply(
+    lambda x: re.sub(r'_categorical_accuracy$', '', x)
 )
-history_df.columns.name = None
+history_df.drop('metric', axis = 1, inplace = True)
+
+#-------------------------------------------------------------------------------
+#   Plot categorical accuracy for each output type across epochs
+#-------------------------------------------------------------------------------
+
+p = (
+    ggplot(
+            history_df,
+            aes(
+                x = 'epoch', y = 'categorical_accuracy', color = 'output_type',
+                group = 'output_type'
+            )
+        ) +
+        geom_line() +
+        facet_wrap('data_group') +
+        coord_cartesian(ylim = [0, 1]) +
+        theme_bw(base_size = 15) +
+        labs(
+            x = 'Epoch Number', y = 'Categorical Accuracy',
+            color = 'Output Type'
+        )
+)
+ggsave(p, filename = metrics_plot_path, width = 10, height = 5)
