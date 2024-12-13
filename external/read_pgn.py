8eab659095027b24c461efe3ff1b1ce15b0ef052
Nick-Eagles <nickeagles77@gmail.com>
2024-12-13 11:10:07 -0500

    Write but don't test yet a function for returning legitimate input and output tensors from external PGN data

--- external/read_pgn.py
+++ external/read_pgn.py
@@ -11,7 +11,15 @@ import misc
 import policy_net
 import Move
 
-#   See 'pgn_to_test_data.py' for an explanation of this function
+#   Given a single game as a string, return input and output tensors for
+#   training a policy-value network. Output tensors are "synthetic" in that
+#   rather than representing good moves and expected rewards, they are
+#   intentionally simple-- see pgn_to_test_data.py for context.
+#
+#   game_str: a string containing an entire game. See preprocess_pgn.R for
+#       conventions on what is expected here
+#   j: (int) game number for reporting back where illegitimate moves occur if
+#       they are encountered
 def game_to_pairs_synthetic(game_str, j):
     move_names = game_str.split(' ')
 
@@ -79,19 +87,18 @@ def game_to_pairs_synthetic(game_str, j):
     
     return (in_vecs, out_vecs)
 
-#   Given a string containing one entire game ('game_str'), the hyperparameter
-#   dictionary 'p', and an 'output_type' of "value": return a 2-tuple where the
-#   first element is a list of (lists of) NN inputs, and the second element is
-#   a list of immediate rewards received for the moves performed. If
-#   'output_type' is "policy_value", return a 3-tuple with the same first two
-#   elements, and the additional list of labels for the third element.
-def generate_raw_pairs(game_str, p, output_type='value'):
-    #   Basic checks on input parameters
-    assert output_type == 'value' or output_type == 'policy_value', output_type
-    
+#   Given a single game as a string, return input and output tensors for
+#   training a policy-value network.
+#
+#   game_str: a string containing an entire game. See preprocess_pgn.R for
+#       conventions on what is expected here
+#   p: a hyperparameter dictionary read in from config.txt
+#   j: (int) game number for reporting back where illegitimate moves occur if
+#       they are encountered
+def game_to_pairs_real(game_str, p, j):
     move_names = game_str.split(' ')
-    
-    stated_result = move_names.pop()[:-1]
+
+    stated_result = move_names.pop()
     if stated_result == '1-0':
         stated_result = 1
     elif stated_result == '0-1':
@@ -101,55 +108,58 @@ def generate_raw_pairs(game_str, p, output_type='value'):
         stated_result = 0
 
     ############################################################################
-    #   Perform the game with the specified moves and receive rewards
+    #   Walk through the game to get a series of games, moves, and rewards
     ############################################################################
+
     game = Game.Game()
-    nn_vecs = []
-    rewards = []
-    out_vecs = []
+    g_list = []
+    move_list = []
+    r_list = []
     for i, move_name in enumerate(move_names):
-        nn_vecs.append(game.toNN_vecs())
-        
         #   Verify that the move played was legal
         moves = board_helper.getLegalMoves(game)
-        actual_names = [m.getMoveName(game.board) for m in moves]
-        if move_name not in actual_names:
-            move_name = fix_move_name(move_name, actual_names, p, game)
-
+        actual_names = [m.getMoveName(game) for m in moves]
+        assert move_name in actual_names, f'game {j}: {move_name}; {" ".join(actual_names)}'
         move_played = moves[misc.match(move_name, actual_names)]
-        if output_type == 'policy_value':
-            g = game.copy()
 
+        move_list.append(move_played)
+        g_list.append(game.copy())
+
+        #   Do move and receive reward
         r = game.getReward(
             move_played, p['mateReward'], simple=True, copy=False
         )[0]
-        rewards.append(r)
-
-        if output_type == 'policy_value':
-            out_vecs.append(policy_net.ToOutputVec(g, move_played, r))
+        r_list.append(r)
 
     ############################################################################
+    #   Update and unravel reward sequence
+    ############################################################################
+
     #   Update final reward, considering resignation as loss and agreed draws
     #   as legitimate draws
-    ############################################################################
-    if game.gameResult != stated_result:
-        if abs(stated_result) == 1:
-            rewards[-1] = stated_result * p['mateReward']
-        else:
-            #   For simplicity, we are neglecting how a potential capture in the
-            #   last move affects the drawing reward (this really should be the
-            #   'bValue' and 'wValue' before performing the last move, to keep
-            #   things precisely consistent, though it shouldn't matter much)
-            rewards[-1] = float(np.log(game.bValue / game.wValue))
-
-    #   Verify lengths of data generated
-    assert len(rewards) == len(move_names), len(move_names)-len(rewards)
-    assert len(nn_vecs) == len(move_names), len(move_names)-len(nn_vecs)
-
-    if output_type == 'policy_value':
-        return (nn_vecs, rewards, out_vecs)
+    if abs(stated_result) == 1:
+        r_list[-1] = stated_result * p['mateReward']
     else:
-        return (nn_vecs, rewards)
+        r_list[-1] = float(np.log(game.bValue / game.wValue))
+    
+    #   Unravel reward, passing it back from the gane's ending with a decay
+    #   factor of gamma
+    for i in range(1, len(r_list)):
+        r_list[-1-i] += p['gamma'] * r_list[-i]
+
+    ############################################################################
+    #   Walk back through game to generate input and output tensors
+    ############################################################################
+
+    in_vecs = []
+    out_vecs = []
+    for i in range(len(r_list)):
+        in_vecs.append(g_list[i].toNN_vecs(every = False)[0])
+        out_vecs.append(
+            policy_net.ToOutputVec(g_list[i], move_list[i], r_list[i])
+        )
+
+    return (in_vecs, out_vecs)
 
 #   Convert many games, as output by generate_raw_pairs or similar, to a tuple
 #   of tensors (X_train, X_test, y_train, y_test).
