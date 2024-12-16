767302b2890ae546c51ef8322e724201371c6ad8
Nick-Eagles <nickeagles77@gmail.com>
2024-12-16 11:08:47 -0500

    Improve readability by making each node a dictionary instead of list (this way each element is named)

--- Traversal.py
+++ Traversal.py
@@ -1,12 +1,7 @@
-import Game
-import board_helper
-import input_handling
-import network_helper
-import misc
 import policy
 
 import numpy as np
-from scipy.special import expit, logit
+from scipy.special import logit
 import tensorflow as tf
 
 class Traversal:
@@ -54,26 +49,32 @@ class Traversal:
         #   One element (node) on the stack is composed of the following:
         #   [moves, rewards, Game, reward up to this, alpha, beta, list of
         #    NN inputs, move names]
-        stack = [[moves, [], self.game, 0, -1 * MAX_R, MAX_R, [], []]]
+        stack = [
+            {
+                'moves': moves, 'rewards': [], 'game': self.game,
+                'prev_reward': 0, 'alpha': -1 * MAX_R, 'beta': MAX_R,
+                'nn_inputs': [], 'move_names': []
+            }
+        ]
         while len(stack) > 0:
             assert len(stack) <= p['depth'] + 1, "Tried to explore too far"
             assert self.nodeHops < self.limit, \
                    "Exceeded the number of node hops required to perform the entire traversal"
-            if len(stack[-1][0]) > 0:   # if there are moves left to explore
+            if len(stack[-1]['moves']) > 0:   # if there are moves left to explore
                 #   alpha-beta pruning
-                if stack[-1][5] <= stack[-1][4]:
+                if stack[-1]['beta'] <= stack[-1]['alpha']:
                     #   None of the remaining lines are relevant in this case
-                    self.pruneCuts += len(stack[-1][0])
-                    stack[-1][0] = []
+                    self.pruneCuts += len(stack[-1]['moves'])
+                    stack[-1]['moves'] = []
                 else:
                     #   Pop the first move and do it
-                    g = stack[-1][2].copy()
-                    m = stack[-1][0].pop(0)
-                    stack[-1][7].append([m.getMoveName(g)])
+                    g = stack[-1]['game'].copy()
+                    m = stack[-1]['moves'].pop(0)
+                    stack[-1]['move_names'].append([m.getMoveName(g)])
                     r = g.getReward(
                         m, p['mateReward'], simple=True, copy=False
                     )[0]
-                    stack[-1][1].append(r)
+                    stack[-1]['rewards'].append(r)
                     
 
                     #   If we aren't at the leaves, compute the next set of
@@ -83,14 +84,17 @@ class Traversal:
                             moves, fullMovesLen = self.policy(self.net, g, p)
                             stack.append(
                                 [
-                                    moves,
-                                    [],
-                                    g,
-                                    stack[-1][3] + p['gamma_exec'] * r,
-                                    stack[-1][4],
-                                    stack[-1][5],
-                                    [],
-                                    []
+                                    {
+                                        'moves': moves,
+                                        'rewards': [],
+                                        'game': g,
+                                        'prev_reward': stack[-1]['prev_reward'] + \
+                                            p['gamma_exec'] * r,
+                                        'alpha': stack[-1]['alpha'],
+                                        'beta': stack[-1]['beta'],
+                                        'nn_inputs': [],
+                                        'move_names': []
+                                    }
                                 ]
                             )
                             self.nodeHops += 1
@@ -99,16 +103,16 @@ class Traversal:
                         else:
                             #   Signal to stop branching here since we found a
                             #   mate (the strongest possible move)
-                            stack[-1][0] = [] 
+                            stack[-1]['moves'] = []
                             self.nodeHops += 2
                     #   At a leaf, we want to add the NN evaluation of the
                     #   position, scaled by our confidence in the NN, to make
                     #   sure rewards are not simply undone later in the game
                     elif self.net.value_certainty > p['minCertainty'] and g.gameResult == 17:
                         in_vec = g.toNN_vecs(every=False)[0]
-                        stack[-1][6].append(in_vec)
+                        stack[-1]['nn_inputs'].append(in_vec)
                     else:
-                        stack[-1][6].append(None)
+                        stack[-1]['nn_inputs'].append(None)
 
             else:   # otherwise hop down one node
                 processNode(stack, self, p)
@@ -122,53 +126,60 @@ def processNode(stack, trav, p):
 
     #   Adjust reward by adding NN evaluations, for whichever positions (if any)
     #   are at max depth and are unfinished games
-    indices = [i for i in range(len(node[6])) if node[6][i] is not None]
+    indices = [
+        i for i in range(len(node['nn_inputs']))
+        if node['nn_inputs'][i] is not None
+    ]
     if len(indices) > 1:
         #   Get the NN evaluations
-        nn_vecs = tf.stack([tf.reshape(node[6][i], [839]) for i in indices])
+        nn_vecs = tf.stack(
+            [tf.reshape(node['nn_inputs'][i], [839]) for i in indices]
+        )
         nn_out = trav.net(nn_vecs, training=False)[-1]
         
         nn_evals = p['gamma_exec'] * trav.net.value_certainty * logit(nn_out)
             
         for i in range(nn_evals.shape[0]):
-            node[1][indices[i]] += float(nn_evals[i])
+            node['rewards'][indices[i]] += float(nn_evals[i])
 
     elif len(indices) == 1:
         #   Get the NN evaluation
-        nn_out = trav.net(node[6][indices[0]], training=False)[-1]
+        nn_out = trav.net(node['nn_inputs'][indices[0]], training=False)[-1]
         
         nn_eval = p['gamma_exec'] * \
                    trav.net.value_certainty * \
                    float(logit(nn_out))
         
-        node[1][indices[0]] += nn_eval
+        node['rewards'][indices[0]] += nn_eval
 
     #   Pass reward down or set trav.baseR (whichever is applicable);
     #   update alpha or beta if applicable
-    if node[2].whiteToMove:
-        index = np.argmax(node[1])
-        r = p['gamma_exec'] * node[1][index]
-        this_line = node[7][index]
+    if node['game'].whiteToMove:
+        index = np.argmax(node['rewards'])
+        r = p['gamma_exec'] * node['rewards'][index]
+        this_line = node['move_names'][index]
         
         #   Update beta if necessary
         if len(stack) > 0:
-            stack[-1][5] = min(stack[-1][5], stack[-1][3] + r)
-
-        
+            stack[-1]['beta'] = min(
+                stack[-1]['beta'], stack[-1]['prev_reward'] + r
+            )
     else:
-        index = np.argmin(node[1])
-        r = p['gamma_exec'] * node[1][index]
-        this_line = node[7][index]
+        index = np.argmin(node['rewards'])
+        r = p['gamma_exec'] * node['rewards'][index]
+        this_line = node['move_names'][index]
 
         #   Update alpha if necessary
         if len(stack) > 0:
-            stack[-1][4] = max(stack[-1][4], stack[-1][3] + r)
+            stack[-1]['alpha'] = max(
+                stack[-1]['alpha'], stack[-1]['prev_reward'] + r
+            )
 
     #   Pass reward and best move name down if applicable
     assert len(this_line) >= 1, len(this_line)
     if len(stack) > 0:
-        stack[-1][1][-1] += r
-        stack[-1][7][-1] += this_line
+        stack[-1]['rewards'][-1] += r
+        stack[-1]['move_names'][-1] += this_line
     else:
         trav.baseR = r / p['gamma_exec']
         trav.bestLine = this_line
