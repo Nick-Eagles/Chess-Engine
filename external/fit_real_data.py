2ef9a837549714de729dc391f216c79d7f7b30f2
Nick-Eagles <nickeagles77@gmail.com>
2024-12-17 15:30:18 -0500

    Train a slightly deeper network on the full dataset once

--- external/fit_real_data.py
+++ external/fit_real_data.py
@@ -16,7 +16,7 @@ from plotnine import ggplot, aes, geom_line, theme_bw, labs, ggsave
 
 train_paths = [
     here('external', 'preprocessed_games', f'tensor_list_train_real{i}.pkl.gz')
-    for i in range(1, 4)
+    for i in range(1, 20)
 ]
 test_path = here(
     'external', 'preprocessed_games', 'tensor_list_test_real.pkl.gz'
@@ -26,7 +26,7 @@ metrics_plot_paths = [
     here('visualization', f'{x}2.pdf')
     for x in ['policy_square', 'policy_end_piece', 'value']
 ]
-hidden_layer_lens = [500, 500]
+hidden_layer_lens = [500, 500, 500, 500]
 policy_weight = 0.5
 optimizer = 'adam'
 batch_size = 100
