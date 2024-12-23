28a7e5987e47a5d3769bd2ec39fd331f3e79711d
Nick-Eagles <nickeagles77@gmail.com>
2024-12-23 14:11:12 -0500

    Bundle network, history, and plots into a single directory for each model. Properly save history to an ongoing list that supports resuming

--- external/fit_real_data.py
+++ external/fit_real_data.py
@@ -3,9 +3,11 @@
 #   network of the current architecture can learn effectively
 
 from pyhere import here
+from pathlib import Path
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
 import numpy as np
 from scipy.special import logit
 import pickle
@@ -14,23 +16,37 @@ import pandas as pd
 import re
 from plotnine import ggplot, aes, geom_line, theme_bw, labs, ggsave
 
-train_paths = [
-    here('external', 'preprocessed_games', f'tensor_list_train_real{i}.pkl.gz')
-    for i in range(1, 20)
-]
-test_path = here(
-    'external', 'preprocessed_games', 'tensor_list_test_real.pkl.gz'
-)
-model_path = here('nets', 'second.keras')
-metrics_plot_paths = [
-    here('visualization', f'{x}2.pdf')
-    for x in ['policy_square', 'policy_end_piece', 'value']
-]
+net_name = "res"
+
+#   If True, load the existing model and history. If False, construct a new
+#   model
+resume = False
+
+#   Hyperparameters (ignored if 'resume' is True)
 hidden_layer_lens = [500, 500, 500, 500]
-policy_weight = 0.5
+loss_weights = [0.4, 0.1, 0.5]
 optimizer = 'adam'
 batch_size = 100
 epochs = 1
+
+train_paths = [
+    here(
+        'external', 'preprocessed_games', 'g75',
+        f'tensor_list_train_real{i}.pkl.gz'
+    )
+    for i in range(1, 20)
+]
+test_path = here(
+    'external', 'preprocessed_games', 'g75', 'tensor_list_test_real.pkl.gz'
+)
+model_path = Path(here('nets', net_name, 'model.keras'))
+history_path = here('nets', net_name, 'history.csv')
+metrics_plot_paths = [
+    here('nets', net_name, f'{x}.pdf')
+    for x in ['policy_square', 'policy_end_piece', 'value']
+]
+
+model_path.parent.mkdir(exist_ok = True)
 
 #   Given input and output tensors and a model 'net', return the cosine
 #   similarity of the model predictions of reward against the labels.
@@ -48,59 +64,62 @@ def cos_sim(X, y, net):
     return result
 
 ################################################################################
-#   Construct a basic neural network
+#   Construct a basic neural network (or load an existing one)
 ################################################################################
 
-#   Input layer
-input_lay = keras.Input(shape = (839,), name = "game_position")
-x = input_lay
+if resume:
+    net = load_model(model_path)
+else:
+    #   Input layer
+    input_lay = keras.Input(shape = (839,), name = "game_position")
+    x = input_lay
 
-#   Dense hidden layers
-for hidden_layer_len in hidden_layer_lens:
-    x = layers.Dense(
-            hidden_layer_len,
-            activation = "relu"
+    #   Dense hidden layers
+    for hidden_layer_len in hidden_layer_lens:
+        x = layers.Dense(
+                hidden_layer_len,
+                activation = "relu"
+            )(x)
+        x = layers.BatchNormalization()(x)
+
+    #   Output layer
+    policy_move_sq = layers.Dense(
+            4096, activation = "softmax", name = "policy_move_square"
         )(x)
-    x = layers.BatchNormalization()(x)
+    policy_end_piece = layers.Dense(
+            6, activation = "softmax", name = "policy_end_piece"
+        )(x)
+    value = layers.Dense(
+            1, activation = "sigmoid", name = "value",
+        )(x)
 
-#   Output layer
-policy_move_sq = layers.Dense(
-        4096, activation = "softmax", name = "policy_move_square"
-    )(x)
-policy_end_piece = layers.Dense(
-        6, activation = "softmax", name = "policy_end_piece"
-    )(x)
-value = layers.Dense(
-        1, activation = "sigmoid", name = "value",
-    )(x)
-
-net = keras.Model(
-    inputs = input_lay,
-    outputs = [policy_move_sq, policy_end_piece, value],
-    name = "network"
-)
+    net = keras.Model(
+        inputs = input_lay,
+        outputs = [policy_move_sq, policy_end_piece, value],
+        name = "network"
+    )
 
 ################################################################################
 #   Compile the model
 ################################################################################
 
-loss_weights = [policy_weight / 2, policy_weight / 2, 1 - policy_weight]
-loss = [
-    tf.keras.losses.CategoricalCrossentropy(), # policy: move sq
-    tf.keras.losses.CategoricalCrossentropy(), # policy: end piece
-    tf.keras.losses.BinaryCrossentropy()       # value
-]
-
-net.compile(
-    optimizer = optimizer,
-    loss = loss,
-    loss_weights = loss_weights,
-    metrics = [
-        tf.keras.metrics.CategoricalAccuracy(),
-        tf.keras.metrics.CategoricalAccuracy(),
-        None
+if not resume:
+    loss = [
+        tf.keras.losses.CategoricalCrossentropy(), # policy: move sq
+        tf.keras.losses.CategoricalCrossentropy(), # policy: end piece
+        tf.keras.losses.BinaryCrossentropy()       # value
     ]
-)
+
+    net.compile(
+        optimizer = optimizer,
+        loss = loss,
+        loss_weights = loss_weights,
+        metrics = [
+            tf.keras.metrics.CategoricalAccuracy(),
+            tf.keras.metrics.CategoricalAccuracy(),
+            None
+        ]
+    )
 
 ################################################################################
 #   Fit the model
@@ -112,6 +131,14 @@ net.compile(
 #   Load the 1000-game test set
 with gzip.open(test_path, 'rb') as f:
     X_test, y_test = pickle.load(f)
+
+#   If resuming, read in the training history. Offset the training step number
+#   later by how many training steps were already performed
+if resume:
+    history_df_old = pd.read_csv(history_path)
+    step_offset = int(history_df_old['training_step'].max())
+else:
+    step_offset = 0
 
 history_df_list = []
 for epoch_num in range(epochs):
@@ -136,7 +163,7 @@ for epoch_num in range(epochs):
         #   rewards (not sure how to do this with keras metrics during fitting)
         history_df = pd.DataFrame(history.history)
         history_df['training_step'] = epoch_num * len(train_paths) + \
-            batch_num + 1
+            batch_num + step_offset + 1
         history_df['value_cos_sim'] = cos_sim(X_train, y_train, net)
         history_df['val_value_cos_sim'] = cos_sim(X_test, y_test, net)
         history_df_list.append(history_df)
@@ -182,6 +209,12 @@ history_df = (
         )
         .reset_index()
 )
+history_df.columns.name = None
+
+if resume:
+    history_df = pd.concat([history_df, history_df_old], axis = 0)
+
+history_df.to_csv(history_path, index = False)
 
 #-------------------------------------------------------------------------------
 #   Plot interesting metrics for policy and value components of outputs
