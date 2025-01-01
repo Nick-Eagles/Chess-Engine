4f37be4370e966a345fbe93047ed2349d1dfb8fc
Nick-Eagles <nickeagles77@gmail.com>
2025-01-01 12:21:30 -0500

    Clean up policy.py based on how functions are actually used now

--- policy.py
+++ policy.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import policy_net
 import misc
 import board_helper
-import Traversal
 
-#################################################################################
+################################################################################
 #   Functions for sampling several legal moves (subsetting search tree)
-#################################################################################
+################################################################################
+
+#   All functions have (net, game, p) as inputs, and output a list of Moves
 
 #   Return the top p['breadth'] moves by evaluation
 def sampleMovesStatic(net, game, p):
@@ -22,92 +23,31 @@ def sampleMovesStatic(net, game, p):
     evals = p['evalFun'](moves, net, game, p)
     return [moves[i] for i in misc.topN(evals, p['breadth'])]
 
-#################################################################################
+################################################################################
 #   Functions for picking a single legal move to play
-#################################################################################
+################################################################################
 
-def returnBestMove(moves, vals, game, num_lines, interactive, best_lines=None, invert=True):
-    if interactive:
-        if game.whiteToMove or not invert:
-            indices = misc.topN(vals, num_lines)
-        else:
-            indices = misc.topN(-1 * vals, num_lines)
+#   All functions have (net, game, p) as inputs, and output a Move
 
-        if best_lines is None:
-            best_line = None
-        else:
-            best_line = best_lines[indices[0]]
-        
-        return (
-            [moves[i] for i in indices],
-            vals[indices],
-            best_line
-        )
-    else:
-        if game.whiteToMove or not invert:
-            bestMove = moves[np.argmax(vals)]
-        else:
-            bestMove = moves[np.argmin(vals)]
-
-        return bestMove
-
-def getBestMoveRawPolicy(net, game, p, interactive=False, num_lines=1):
+def getBestMoveRawPolicy(net, game, p):
     moves = board_helper.getLegalMoves(game)
     
     #   Compute a probability distribution across legal moves
     outputs = net(game.toNN_vecs(), training=False)[:2]
     probs = policy_net.AdjustPolicy(outputs, moves, game)
 
-    return returnBestMove(
-        moves, probs, game, num_lines, interactive, None, False
-    )
-        
+    return moves[np.argmax(probs)]
 
-def getBestMoveTreeEG(net, game, p, interactive=False, num_lines=1):
-    #   Traversals are started at positions resulting from testing moves
-    #   from the current position; this test constitutes a step of depth
-    p_copy = p.copy()
-    p_copy['depth'] -= 1
-    p = p_copy
-    
-    moves = sampleMovesStatic(net, game, p)
-    
-    rTemp = np.zeros(len(moves))
-    baseRs = np.zeros(len(moves))
+################################################################################
+#   Functions for evaluating all legal moves
+################################################################################
 
-    if interactive:
-        bestLines = []
-    else:
-        bestLines = None
+#   All functions have (moves, net, game, p) as inputs and return a numpy
+#   array (same length as number of moves) of evaluations. Best evaluations are
+#   always larger, regardless of the player to move
 
-    for i, m in enumerate(moves):
-        g = game.copy()
-        g.quiet = True
-            
-        rTemp[i] = g.getReward(
-            m, p['mateReward'], simple=True, copy=False
-        )[0]
-        t = Traversal.Traversal(g, net, p)
-        t.traverse()
-        baseRs[i] = t.baseR
-
-        if interactive:
-            #   Add a list of move names for the top line explored that
-            #   started with this particular move "m"
-            bestLines.append([m.getMoveName(game)] + t.bestLine)
-
-    rTemp += p['gamma_exec'] * baseRs
-
-    return returnBestMove(
-        moves, rTemp, game, num_lines, interactive, bestLines, True
-    )
-
-
-#   A helper function to compute depth-1 evaluations of a list of moves. Returns
-#   a list the same length as 'moves', with each component being the sum of the
-#   immediate reward for performing the respective move and a scaled
-#   NN-evaluation of the resulting position. Note that evals are flipped: larger
-#   values match to better moves for the current player!
+#   Simply try each move and check the network's value at the resulting
+#   positions
 def getEvalsValue(moves, net, game, p):
     #   Compute NN evaluations on each move
     r_real = np.zeros(len(moves))
@@ -128,6 +68,7 @@ def getEvalsValue(moves, net, game, p):
     assert evals.shape == (len(moves),), evals.shape
     return evals
 
+#   Evaluations directly use the policy piece of the network's output
 def getEvalsPolicy(moves, net, game, p):
     #   Compute a probability distribution across legal moves
     outputs = net(game.toNN_vecs(), training=False)[:2]
@@ -135,6 +76,9 @@ def getEvalsPolicy(moves, net, game, p):
     
     return evals
 
+#   Evaluations weight the network policy against the observed rewards for
+#   performing each move. The policy piece dominates when the best observed
+#   reward is low, but observed rewards dominate when the largest is large
 def getEvalsHybrid(moves, net, game, p):
     #   Compute a probability distribution across legal moves
     outputs = net(game.toNN_vecs(), training=False)[:2]
